ccb0da106240342b76d9f415528241457e297c50
Herman <germanstashinskii@gmail.com>
2019-03-04 23:22:30 +0300

    Helpers and other stuff were added

--- SystemRemoteLogger.Services/UdpConnectionService.cs
+++ SystemRemoteLogger.Services/UdpConnectionService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using SystemRemoteLogger.Services.Helpers;
 
 namespace SystemRemoteLogger.Services
 {
@@ -43,6 +44,50 @@ namespace SystemRemoteLogger.Services
             senderUdpClient.JoinMulticastGroup(multiCastAddress);
             remoteEndPoint = new IPEndPoint(multiCastAddress, port);
             //SendMessage("connected to chat");
+        }
+
+        private void SendMessage(string message)
+        {
+            try
+            {
+                string formattedMesage = string.Format($"{userName} : {message}");
+                byte[] buffer = EncryptionHelper.Encode(formattedMesage);
+                senderUdpClient.Send(buffer, buffer.Length, remoteEndPoint);
+            }
+            catch (Exception ex)
+            {
+                //TODO: Handle exception
+                //MessageBox.Show(ex.Message, "Error while sending message");
+            }
+        }
+
+        private void Listen()
+        {
+            try
+            {
+                recievingUdpClient = new UdpClient();
+                IPEndPoint localEp = new IPEndPoint(IPAddress.Any, port);
+
+                //SO_REUSEADDR 
+                recievingUdpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                recievingUdpClient.ExclusiveAddressUse = false;
+
+                recievingUdpClient.Client.Bind(localEp);
+
+                recievingUdpClient.JoinMulticastGroup(multiCastAddress);
+
+                while (IsConnectionAlive)
+                {
+                    byte[] data = recievingUdpClient.Receive(ref localEp);
+                    string message = EncryptionHelper.Decode(data);
+                    //Do smth!!!
+                }
+            }
+            catch (Exception ex)
+            {
+                //catch
+                //MessageBox.Show(ex.Message, "Error while listeting port");
+            }
         }
     }
 }
