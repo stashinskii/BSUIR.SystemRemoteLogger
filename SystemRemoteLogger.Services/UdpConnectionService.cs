f8e81a817000f51896c3aa5521590f8c3e2b4820
Herman <germanstashinskii@gmail.com>
2019-03-04 19:26:28 +0300

    Initialization of UDP Service Properties

---
+++ SystemRemoteLogger.Services/UdpConnectionService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SystemRemoteLogger.Services
+{
+    public class UdpConnectionService
+    {
+        #region UDP clients
+        private UdpClient senderUdpClient;
+        private UdpClient recievingUdpClient;
+        #endregion
+
+        #region Addresses
+        private IPAddress multiCastAddress;
+        private IPEndPoint remoteEndPoint;
+        #endregion
+
+        #region Configuration
+        private const int port = 65000;
+        private bool IsConnectionAlive;
+        private string userName = "Current OS";
+        #endregion
+    }
+}
