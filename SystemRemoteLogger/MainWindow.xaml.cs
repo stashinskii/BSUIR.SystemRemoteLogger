5f23e512b2556ed0db8efcf3aed131d2483c720f
Herman <germanstashinskii@gmail.com>
2019-03-04 19:10:19 +0300

    Initial commit

---
+++ SystemRemoteLogger/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace SystemRemoteLogger.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
