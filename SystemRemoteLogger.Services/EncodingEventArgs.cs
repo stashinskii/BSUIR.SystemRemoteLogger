5e990261f9a351c7c584085fb07a37f7f9062442
Herman <hermanstashinskii@yandex.ru>
2019-03-25 09:19:00 +0300

    Udp Client DONE

---
+++ SystemRemoteLogger.Services/EncodingEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemRemoteLogger.Services
+{
+    public class EncodingEventArgs : EventArgs
+    {
+        public byte[] dataToDecode;
+        public EncodingEventArgs() : base()
+        {
+        }
+    }
+}
